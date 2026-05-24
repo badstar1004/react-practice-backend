e502569fd661b03189e1e6bfdc7344cfbd0e5f2a
김은정 <dmswjd4566@hanmail.net>
2026-05-25 01:07:38 +0900

    Initial commit

---
+++ src/main/java/com/enough/issuebe/IssueBeApplication.java
@@ -0,0 +1,13 @@
+package com.enough.issuebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssueBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enough/issuebe/IssueBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enough.issuebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
